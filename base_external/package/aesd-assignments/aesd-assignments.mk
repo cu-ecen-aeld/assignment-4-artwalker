dd680895371e119b720e3744d676f3f7843fe973
artwalker <xinxingwang@acm.org>
2025-01-24 16:15:31 +0800

    Update to the daemon

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,12 +7,8 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 # AESD_ASSIGNMENTS_VERSION = 1f4c327172c1ae54bfe29dbf3a14c4a5ef1f30fd
-<<<<<<< HEAD
 # AESD_ASSIGNMENTS_VERSION = 421b03d25f343fa320d9a937d7fa00c32b23e761
-AESD_ASSIGNMENTS_VERSION = 089c0e6c87bbe9870c214a70c4a1cd787d59fb4b
-=======
-AESD_ASSIGNMENTS_VERSION = 421b03d25f343fa320d9a937d7fa00c32b23e761
->>>>>>> refs/remotes/assignment-4-remote/main
+AESD_ASSIGNMENTS_VERSION = 0b68876d6628794ca4225126df113d4020740a21
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
